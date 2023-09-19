c2106f4452c1886fcfe6aa17d27bf08cc56c00aa
EEEP Gerardo José Dias de Loiola <139022101+EEEP-Gerardo@users.noreply.github.com>
2023-09-19 12:32:04 -0300

    add new second commit
    Adicionado o 2° questão, jogo de adivinhação

---
+++ src/index_2.cs
@@ -0,0 +1,44 @@
+// 2° Jogo de Adivinhação
+
+using System;
+
+namespace JogoAdivinhacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo ao Jogo de Adivinhação!");
+            
+            // Gere um número aleatório entre 1 e 100
+            Random random = new Random();
+            int numeroAleatorio = random.Next(1, 101);
+
+            int tentativas = 0;
+            bool acertou = false;
+
+            do
+            {
+                Console.Write("Tente adivinhar o número: ");
+                int palpite = int.Parse(Console.ReadLine());
+                tentativas++;
+
+                if (palpite == numeroAleatorio)
+                {
+                    acertou = true;
+                }
+                else if (palpite < numeroAleatorio)
+                {
+                    Console.WriteLine("Tente um número maior.");
+                }
+                else
+                {
+                    Console.WriteLine("Tente um número menor.");
+                }
+            } while (!acertou);
+
+            Console.WriteLine($"Parabéns! Você acertou o número {numeroAleatorio} em {tentativas} tentativas.");
+        }
+    }
+}
+
