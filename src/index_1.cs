17a3754d8061c830ee331f3983be655cff80bb7e
EEEP Gerardo José Dias de Loiola <139022101+EEEP-Gerardo@users.noreply.github.com>
2023-09-19 12:25:29 -0300

    add new: initial commit
    adicionado o primeiro exercício de programação utilizado a linguagem csharp

---
+++ src/index_1.cs
@@ -0,0 +1,44 @@
+// 1° exercício de programação orientada a objeto: calcular média
+
+using System;
+
+namespace CalculoMediaAlunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de Média de Notas");
+            
+            // Solicita o número de notas
+            Console.Write("Quantas notas você deseja calcular? ");
+            int numeroDeNotas = int.Parse(Console.ReadLine());
+
+            double somaDasNotas = 0;
+
+            // Loop para coletar as notas
+            for (int i = 1; i <= numeroDeNotas; i++)
+            {
+                Console.Write($"Digite a nota {i}: ");
+                double nota = double.Parse(Console.ReadLine());
+                somaDasNotas += nota;
+            }
+
+            // Calcula a média
+            double media = somaDasNotas / numeroDeNotas;
+
+            // Exibe a média
+            Console.WriteLine($"A média das notas é: {media:F2}");
+
+            // Verifica se o aluno passou
+            if (media >= 6.0)
+            {
+                Console.WriteLine("O aluno foi aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("O aluno foi reprovado.");
+            }
+        }
+    }
+}
